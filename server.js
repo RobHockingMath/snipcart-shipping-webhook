382b5e5901df45507984c867628868f3c8228cb5
Rob Hocking <rob.l.hocking@gmail.com>
2025-03-07 19:40:46 +0800

    updated to server.js to not hardcode a thing

--- server.js
+++ server.js
@@ -292,5 +292,7 @@ app.post("/shippingrates", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Shipping webhook running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Shipping webhook running on port ${PORT}`));
+
 
